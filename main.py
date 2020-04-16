6363fe6036b3c7a5ff1de36dfa801b9a6c622741
Aitor Arronte <aitoraalvarez@gmail.com>
2020-04-15 22:29:52 -1000

    cuda to device

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import torch
 train_data, test_data = get_data()
 model = CRNN()
 if torch.cuda.is_available():
-    model.CUDA()
+    device = torch.device('cuda:0')
+else:
+    device = torch.device('cpu')
+model.to(device)
 train.train_model(model, train_data)
 model.save()
 train.test_model(model, test_data)
