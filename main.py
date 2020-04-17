c532373347d84eb6894000bb4eef94e9e17dbbb0
Aitor Arronte <aitoraalvarez@gmail.com>
2020-04-17 11:49:19 -1000

    added residual parameters to model

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from utils.dataloader import get_data
 from models.crnn import CRNN
-from models.residual import ResidualLSTM
+from models.residual import ResidualLSTM, Resblock
 from models import train
 import torch
 
 train_data, test_data = get_data()
-model = CRNN()
+#model = CRNN()
+model = ResidualLSTM(Resblock, [2])
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
 else:
